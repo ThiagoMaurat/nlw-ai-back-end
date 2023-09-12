7e4ce04d594c9e846cd6cb847dfd522065c6c44c
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-09-12 20:26:17 -0300

    feat:upload video route

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
-import {fastify} from 'fastify';
+import { fastify } from "fastify";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify();
 
-app.get('/', (request, reply) => {
-    return 'Hello World'
-})
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
 
-app.listen({port: 3333}, (err, address) => {
-    if(err) {
-        console.log(err)
-        process.exit(1)
-    }
-    console.log(`Server running at ${address}`)
-})
+app.listen({ port: 3333 }, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server running at ${address}`);
+});
