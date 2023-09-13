69dcbe4e03e17d1b13904886d4b9e945a16cfc0b
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-09-12 21:22:07 -0300

    feat:generate ai completion route

---
+++ src/routes/generate-ai-completion.ts
@@ -0,0 +1,46 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
+
+export async function generateAICompletionRoute(app: FastifyInstance) {
+  app.post("/ai/complete", async (request, reply) => {
+    const paramsSchema = z.object({
+      videoId: z.string(),
+      template: z.string(),
+      temperature: z.number().min(0).max(10).default(0.5),
+    });
+
+    const { temperature, template, videoId } = paramsSchema.parse(
+      request.params
+    );
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      },
+    });
+
+    if (!video) {
+      return reply.status(400).send("Video not found");
+    }
+
+    const promptMessage = template.replace(
+      "{transcription}",
+      video.transcription ?? ""
+    );
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      temperature: temperature,
+      messages: [
+        {
+          role: "user",
+          content: promptMessage,
+        },
+      ],
+    });
+
+    return response;
+  });
+}
